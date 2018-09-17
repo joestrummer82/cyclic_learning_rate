416796685b24d3f8def75dfe1b88009e1d30282a
Christian Kungel <christian.kungel@zeiss.com>
2018-09-17 10:21:22 +0200

    pep8 conformity

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Implementation of cyclic learning rate from the paper: [Smith, Leslie N. "Cyclic
 ## What is CLR?
 CLR is used to enhance the way the learning rate is scheduled during training, to provide better convergence and help in regularizing deep learning models.
 It eliminates the need to experimentally find the best values for the global learning rate. Allowing the learning rate to cyclically vary between lower and upper boundary values.
-The idea is to divide the training process into cycles determined by a stepsize parameter, which defines the number of iterations in half a cycle.
+The idea is to divide the training process into cycles determined by a *stepsize* parameter, which defines the number of iterations in half a cycle.
--- tensorflow/cyclic_lr_test.py
+++ tensorflow/cyclic_lr_test.py
@@ -6,62 +6,62 @@ from tensorflow.python.framework import test_util
 
 
 class TestCyclicLearningRate(test_util.TensorFlowTestCase):
-  """Functional test for Cyclic Learning Rate"""
+    """Functional test for Cyclic Learning Rate"""
 
-  def np_cyclic_learning_rate(self, step, lr, max_lr, step_size, mode):
-    cycle = math.floor(1. + step / (2. * step_size))
-    x = math.fabs(step / step_size - 2. * cycle + 1.)
-    clr = (max_lr - lr) * max(0., 1. - x)
-    if mode == 'triangular2':
-      clr = clr/(math.pow(2, (cycle - 1)))
-    if mode == 'exp_range':
-      clr = clr * math.pow(.99994, step)
-    return clr + lr
+    def np_cyclic_learning_rate(self, step, lr, max_lr, step_size, mode):
+        cycle = math.floor(1. + step / (2. * step_size))
+        x = math.fabs(step / step_size - 2. * cycle + 1.)
+        clr = (max_lr - lr) * max(0., 1. - x)
+        if mode == 'triangular2':
+            clr = clr / (math.pow(2, (cycle - 1)))
+        if mode == 'exp_range':
+            clr = clr * math.pow(.99994, step)
+        return clr + lr
 
-  @test_util.run_in_graph_and_eager_modes()
-  def test_triangular(self):
-    step = 5
-    lr = 0.01
-    max_lr = 0.1
-    step_size = 20.
-    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
-                                                         lr, max_lr,
-                                                         step_size,
-                                                         mode='triangular')
-    expected = self.np_cyclic_learning_rate(step,
-                                            lr, max_lr,
-                                            step_size,
-                                            mode='triangular')
-    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+    @test_util.run_in_graph_and_eager_modes()
+    def test_triangular(self):
+        step = 5
+        lr = 0.01
+        max_lr = 0.1
+        step_size = 20.
+        cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                             lr, max_lr,
+                                                             step_size,
+                                                             mode='triangular')
+        expected = self.np_cyclic_learning_rate(step,
+                                                lr, max_lr,
+                                                step_size,
+                                                mode='triangular')
+        self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
 
-  @test_util.run_in_graph_and_eager_modes()
-  def test_triangular2(self):
-    step = 5
-    lr = 0.01
-    max_lr = 0.1
-    step_size = 20.
-    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
-                                                         lr, max_lr,
-                                                         step_size,
-                                                         mode='triangular2')
-    expected = self.np_cyclic_learning_rate(step,
-                                            lr, max_lr,
-                                            step_size,
-                                            mode='triangular2')
-    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+    @test_util.run_in_graph_and_eager_modes()
+    def test_triangular2(self):
+        step = 5
+        lr = 0.01
+        max_lr = 0.1
+        step_size = 20.
+        cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                             lr, max_lr,
+                                                             step_size,
+                                                             mode='triangular2')
+        expected = self.np_cyclic_learning_rate(step,
+                                                lr, max_lr,
+                                                step_size,
+                                                mode='triangular2')
+        self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
 
-  @test_util.run_in_graph_and_eager_modes()
-  def test_exp_range(self):
-    step = 5
-    lr = 0.01
-    max_lr = 0.1
-    step_size = 20.
-    cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
-                                                         lr, max_lr,
-                                                         step_size,
-                                                         mode='exp_range')
-    expected = self.np_cyclic_learning_rate(step,
-                                            lr, max_lr,
-                                            step_size,
-                                            mode='exp_range')
-    self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
+    @test_util.run_in_graph_and_eager_modes()
+    def test_exp_range(self):
+        step = 5
+        lr = 0.01
+        max_lr = 0.1
+        step_size = 20.
+        cyclic_lr = learning_rate_decay.cyclic_learning_rate(step,
+                                                             lr, max_lr,
+                                                             step_size,
+                                                             mode='exp_range')
+        expected = self.np_cyclic_learning_rate(step,
+                                                lr, max_lr,
+                                                step_size,
+                                                mode='exp_range')
+        self.assertAllClose(self.evaluate(cyclic_lr), expected, 1e-6)
